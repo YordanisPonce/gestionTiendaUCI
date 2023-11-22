c7474be5dda030cc4915160acdb0da5a59bc5def
Yordis <yordisponcealderete@gmail.com>
2023-11-22 16:30:34 -0500

    Test suite to review

--- tests/Feature/AreaTest.php
+++ tests/Feature/AreaTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Area;
 
 class AreaTest extends TestCase
 {
@@ -13,10 +14,44 @@ class AreaTest extends TestCase
      *
      * @return void
      */
-    public function test_get_areas()
+    public function test_user_can_not_get_areas()
     {
         $response = $this->get('/areas');
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('login');
+    }
 
+    public function test_user_can_get_areas()
+    {
+        $response = $this->actingAs($this->superAdmin)->get('/areas');
         $response->assertStatus(200);
+        $response->assertViewHas('areas');
+    }
+
+    public function test_user_can_delete_areas()
+    {
+        $area = Area::first();
+        $response = $this->actingAs($this->superAdmin)->delete(route('areas.destroy', $area));
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('areas.index');
+    }
+
+    public function test_user_can_create_areas()
+    {
+        $response = $this->actingAs($this->superAdmin)->post(route('areas.store'), 
+        ['name' => 'AreaTestCreate','workers_count' => 25]
+    );
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('areas.index');
+    }
+
+    public function test_user_can_update_areas()
+    {
+        $area = Area::first();
+        $response = $this->actingAs($this->superAdmin)->put(route('areas.update' , $area), 
+        ['name' => 'AreaTestUpdate','workers_count' => 24]
+    );
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('areas.index');
     }
 }
--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Product;
+
 
 class ProductTest extends TestCase
 {
@@ -13,9 +15,52 @@ class ProductTest extends TestCase
      *
      * @return void
      */
-    public function test_products()
-    {        $response = $this->get('/products');
+    public function test_user_can_not_get_products()
+    {
+        $response = $this->get('/products');
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('login');
+    }
 
+    public function test_user_can_get_products()
+    {
+        $response = $this->actingAs($this->superAdmin)->get('/products');
         $response->assertStatus(200);
+        $response->assertViewHas('products');
+    }
+
+    public function test_user_can_delete_products()
+    {
+        $product = Product::firstOrCreate(['name' => 'ProductTest','price' => 25, 'format' => '1 unidad']);
+        $response = $this->actingAs($this->superAdmin)->delete(route('products.destroy', $product));
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('products.index');
+    }
+
+    public function test_user_can_create_products()
+    {
+        $response = $this->actingAs($this->superAdmin)->post(route('products.store'), 
+        ['name' => 'ProductTest','price' => 25, 'format' => '1 unidad']
+    );
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('products.index');
+    }
+
+    public function test_user_can_update_products()
+    {
+        $product = Product::first();
+        $response = $this->actingAs($this->superAdmin)->put(route('products.update' , $product), 
+        ['name' => 'ProductTest','price' => 25, 'format' => '1 unidad']
+    );
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('products.index');
+    }
+
+    public function test_user_can_asign_product()
+    {
+        $product= Product::first();
+        $response = $this->actingAs($this->superAdmin)->post(route('products.asignProduct' , $product));
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('products.index');
     }
 }
